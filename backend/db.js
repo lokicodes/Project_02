d8d54c33cccd47083868756748e8644cd2ff758d
lokicodes <lokeshsharma.20020219@gmail.com>
2022-03-09 19:08:46 +0530

    created schemas and routes

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,16 @@
 // mongo "mongodb+srv://cluster0.yctsz.mongodb.net/myFirstDatabase" --username loki_codes
 const mongoose = require("mongoose");
-const mongoURI = "mongodb+srv://cluster0.yctsz.mongodb.net/myFirstDatabase";
+const mongoURI = "mongodb+srv://loki_codes:lokesh%40123@cluster0.yctsz.mongodb.net/TestDB?retryWrites=true&w=majority";
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("ho gaya mongoose se connect!");
-    })
+    // mongoose.connect(mongoURI, ()=>{
+    //     console.log("ho gaya mongoose se connect!");
+    // })
+    mongoose.connect(
+        mongoURI,
+        (err) => {
+         if(err) console.log(err) 
+         else console.log("mongdb is connected");
+        }
+      );
 }
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,14 @@ const express = require('express');
 connectToMongo();
 const app = express() ;
 const port = 5000 ;
+app.use(express.json());
 
 // Available Routes
 app.use("/api/auth" , require("./routes/auth")) ;
 app.use("/api/blogs" , require("./routes/blogs")) ;
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('hello world');
 })
 
 app.listen(port, ()=>{
