71dde6b0cbc7073ac7f43d1c5d14d5c8bfef35c1
lokicodes <lokeshsharma.20020219@gmail.com>
2022-06-03 00:06:01 +0530

    deployed

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(express.json());
 app.use("/api/auth" , require("./routes/auth")) ;
 app.use("/api/blogs" , require("./routes/blogs")) ;
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// })
 
 
 // deploy 
