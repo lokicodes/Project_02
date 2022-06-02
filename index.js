35f8cd290fae21c5462c8fe2f22c6f6d3569198b
lokicodes <lokeshsharma.20020219@gmail.com>
2022-06-02 23:54:18 +0530

    test commit 3

---
+++ index.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv") ;
+const connectToMongo = require('./db');
+const express = require('express');
+var cors = require("cors");
+connectToMongo();
+const app = express() ;
+
+dotenv.config({path: './.env'}) ;
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Available Routes
+app.use("/api/auth" , require("./routes/auth")) ;
+app.use("/api/blogs" , require("./routes/blogs")) ;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
+
+
+// deploy 
+if ( process.env.NODE_ENV == "production"){
+  app.use(express.static("frontend/build"));
+}
+app.listen(port, ()=>{
+  console.log(`server started at port ${port}`);
+})
